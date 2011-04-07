532ec84f8bb3d1ff4ff7c5a5f80ee6841b9f1783
Kate Hedstrom <kshedstrom@alaska.edu>
2011-04-07 14:48:26 -0800

    POP grid changes.

--- pyroms_toolbox/pyroms_toolbox/BGrid_POP/BGrid_POP.py
+++ pyroms_toolbox/pyroms_toolbox/BGrid_POP/BGrid_POP.py
@@ -16,19 +16,16 @@ class BGrid_POP(object):
 
 
     def __init__(self, lon_t, lat_t, lon_uv, lat_uv, \
-                       mask_t, z_t, angle, name):
+                       mask_t, mask_uv, z_t, angle, name):
 
         self.name = name
-
-        self.xrange = xrange
-        self.yrange = yrange
 
         self.lon_t = lon_t
         self.lat_t = lat_t
         self.lon_uv = lon_uv
         self.lat_uv = lat_uv
         self.mask_t = mask_t
-#        self.mask_uv = mask_uv
+        self.mask_uv = mask_uv
 #        self.h = h
         self.z_t = z_t
 #        self.z_t_edges = z_t_edges
@@ -36,14 +33,67 @@ class BGrid_POP(object):
 #        self.z_uv_edges = z_uv_edges
 #        self.f = f
 
-        self.lon_t_vert = lon_uv
-        self.lat_t_vert = lat_uv
-
-        self.lon_uv_vert = lon_t
-        self.lat_uv_vert = lat_t
+        self._calculate_t_vert()
+        self._calculate_uv_vert()
 
         self.angle = angle
 
+    def _calculate_t_vert(self):
+        Mm, Lm = self.lon_t.shape
+        lon = np.zeros((Mm+1,Lm+1))
+        lat = np.zeros((Mm+1,Lm+1))
+
+        lon[1:, 1:] = self.lon_uv[:,:]
+        lat[1:, 1:] = self.lat_uv[:,:]
+
+        #South edge
+        lon[0,0:-1] = self.lon_t[0,:] - ( self.lon_uv[0,:] - self.lon_t[0,:] )
+        lon[0,-1] = self.lon_t[0,-1] - ( self.lon_uv[0,-2] - self.lon_t[0,-1] )
+        lat[0,0:-1] = self.lat_t[0,:] - ( self.lat_uv[0,:] - self.lat_t[0,:] )
+        lat[0,-1] = self.lat_t[0,-1] - ( self.lat_uv[0,-2] - self.lat_t[0,-1] )
+
+        #West edge
+        lon[0:-1,0] = self.lon_t[:,0] - ( self.lon_uv[:,0] - self.lon_t[:,0] )
+        lon[-1,0] = self.lon_t[-1,0] - ( self.lon_uv[-2,0] - self.lon_t[-1,0] )
+        lat[0:-1,0] = self.lat_t[:,0] - ( self.lat_uv[:,0] - self.lat_t[:,0] )
+        lat[-1,0] = self.lat_t[-1,0] - ( self.lat_uv[-2,0] - self.lat_t[-1,0] )
+
+        self.lon_t_vert = lon
+	self.lat_t_vert = lat
+
+
+    def _calculate_uv_vert(self):
+        Mm, Lm = self.lon_uv.shape
+        lon = np.zeros((Mm+1,Lm+1))
+        lat = np.zeros((Mm+1,Lm+1))
+
+        lon[:-1, :-1] = self.lon_t[:,:]
+        lat[:-1, :-1] = self.lat_t[:,:]
+
+        #North edge
+        lon[-1,0:-2] = self.lon_uv[-1,:-1] - ( self.lon_t[-1,1:] - self.lon_uv[-1,:-1] )
+        lon[-1,-2:] = self.lon_uv[-1,-2:] - ( self.lon_t[-1,-2:] - self.lon_uv[-1,-2:] )
+        lat[-1,0:-2] = self.lat_uv[-1,:-1] - ( self.lat_t[-1,1:] - self.lat_uv[-1,:-1] )
+        lat[-1,-2:] = self.lat_uv[-1,-2:] - ( self.lat_t[-1,-2:] - self.lat_uv[-1,-2:] )
+
+        #East edge
+        lon[0:-2,-1] = self.lon_uv[:-1,-1] - ( self.lon_t[1:,-1] - self.lon_uv[:-1,-1] )
+        lon[-2,-1] = self.lon_uv[-2:-1,-1] - ( self.lon_t[-2:-1,-1] - self.lon_uv[-2:-1,-1] )
+        lat[0:-2,-1] = self.lat_uv[:-1,-1] - ( self.lat_t[1:,-1] - self.lat_uv[:-1,-1] )
+        lat[-2,-1] = self.lat_uv[-2:-1,-1] - ( self.lat_t[-2:-1,-1] - self.lat_uv[-2:-1,-1] )
+
+        self.lon_uv_vert = lon
+        self.lat_uv_vert = lat
+
+
+    def _calculate_grid_angle(self):
+        geod = pyproj.Geod(ellps='WGS84')
+        az_forward, az_back, dx = geod.inv(self.lon_t[:,:-1], self.lat_t[:,:-1], \
+                                           self.lon_t[:,1:], self.lat_t[:,1:])
+
+        angle = 0.5 * (az_forward[1:,:] + az_forward[:-1,:])
+        self.angle = (90 - angle) * np.pi/180.
+        
 
     def _calculate_grid_angle(self):
         geod = pyproj.Geod(ellps='WGS84')
--- pyroms_toolbox/pyroms_toolbox/BGrid_POP/get_nc_BGrid_POP.py
+++ pyroms_toolbox/pyroms_toolbox/BGrid_POP/get_nc_BGrid_POP.py
@@ -36,8 +36,8 @@ def get_nc_BGrid_POP(grdfile, name='POP_CHUKCHI'):
     N_t = dz.shape[0]
     dz = dz * 0.01
     z_t = dz
-    for k in range(1:N_t):
-        z_t(k) = z_t(k-1) + dz(k)
+    for k in range(1,N_t):
+        z_t[k] = z_t[k-1] + dz[k]
 
     mask_t = np.zeros((N_t, M_t, L_t))
     for j in range(M_t):
@@ -48,18 +48,21 @@ def get_nc_BGrid_POP(grdfile, name='POP_CHUKCHI'):
                 mask_t[:, j,i] = 0
 
     # compute mask at uv-point
-    M_uv, L_uv = kmu.shape
-    N_uv = z_uv.shape[0]
+#    M_uv, L_uv = kmu.shape
+    M_uv, L_uv = kmt.shape
+    N_uv = dz.shape[0]
     mask_uv = np.zeros((N_uv, M_uv, L_uv))
     for j in range(M_uv):
         for i in range(L_uv):
             try:
-                mask_uv[0:kmu[j,i], j,i] = 1
+                mask_uv[0:kmt[j,i], j,i] = 1
+                mask_uv[0,j,i] = min(mask_t[0,j,i], mask_t[0,j+1,i], \
+                    mask_t[0,j,i+1],mask_t[0,j+1,i+1])
             except:
                 mask_uv[:, j,i] = 0
 
     return BGrid_POP(lon_t, lat_t, lon_uv, lat_uv, \
-                       mask_t, z_t, angle, name)
+                       mask_t, mask_uv, z_t, angle, name)
 #    return BGrid_POP(lon_t, lat_t, lon_uv, lat_uv, \
 #                       mask_t, mask_uv, h, z_t, z_t_edges, \
 #                       z_uv, z_uv_edges, f, \
