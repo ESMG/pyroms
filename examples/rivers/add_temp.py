2e831733924f32e68c7649812a05ba7d9754a55a
Kate Hedstrom <kshedstrom@alaska.edu>
2015-05-26 13:52:25 -0800

    Uncommented some lines in add_temp.py

--- examples/rivers/add_temp.py
+++ examples/rivers/add_temp.py
@@ -28,21 +28,21 @@ for line in f:
 # create file with all the objects
 out = netCDF4.Dataset(outfile, 'a', format='NETCDF3_64BIT')
 
-#out.createDimension('river_tracer_time', len(ttime))
+out.createDimension('river_tracer_time', len(ttime))
 
-#times = out.createVariable('river_tracer_time', 'f8', ('river_tracer_time'))
-#times.units = 'day'
-#times.cycle_length = 365.25
-#times.long_name = 'river tracer time'
+times = out.createVariable('river_tracer_time', 'f8', ('river_tracer_time'))
+times.units = 'day'
+times.cycle_length = 365.25
+times.long_name = 'river tracer time'
 
-#temp = out.createVariable('river_temp', 'f8', ('river_tracer_time'))
-#temp.long_name = 'river runoff potential temperature'
-#temp.units = 'Celsius'
-#temp.time = 'river_tracer_time'
+temp = out.createVariable('river_temp', 'f8', ('river_tracer_time'))
+temp.long_name = 'river runoff potential temperature'
+temp.units = 'Celsius'
+temp.time = 'river_tracer_time'
 
-#salt = out.createVariable('river_salt', 'f8', ('river_tracer_time'))
-#salt.long_name = 'river runoff salinity'
-#salt.time = 'river_tracer_time'
+salt = out.createVariable('river_salt', 'f8', ('river_tracer_time'))
+salt.long_name = 'river runoff salinity'
+salt.time = 'river_tracer_time'
 
 out.variables['river_tracer_time'][:] = ttime
 out.variables['river_temp'][:] = temp
